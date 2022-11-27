49e146197f5d5fb4e224275d5918976b622435aa
mariotawfik <amxmaroz@gmail.com>
2022-11-27 15:59:20 +0200

    Initial Commit

---
+++ DS2Final/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DS2Final
+//
+//  Created by Mary Tamry on 27.11.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
